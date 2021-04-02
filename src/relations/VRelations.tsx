945efca5637d0a702f24a99a6c6c3fe7dc2575a3
Henry <liaohengyi@outlook.com>
2021-04-02 00:29:01 -0400

    relations

--- src/relations/VRelations.tsx
+++ src/relations/VRelations.tsx
@@ -1,12 +1,20 @@
-import { VPage } from "tonva-react";
+import { FA, LMR, VPage } from "tonva-react";
 import { CRelations } from "./CRelations";
 
 export class VRelations extends VPage<CRelations> {
 	header() {return '客户'}
-	content() {
+	content() {		
 		return <div className="">
-			<div className="px-3 py-2 mb-1 cursor-pointer bg-white" onClick={this.controller.showUnits}>机构</div>
-			<div className="px-3 py-2 mb-1 cursor-pointer bg-white" onClick={this.controller.showCostomers}>客户</div>
+			<LMR className="px-3 py-2 mb-1 cursor-pointer bg-white align-items-center" 
+				left={<FA name="home" className="text-primary mr-3" />}
+				onClick={this.controller.showUnits}>
+				机构
+			</LMR>
+			<LMR className="px-3 py-2 mb-1 cursor-pointer bg-white align-items-center"
+				left={<FA name="user" className="text-info mr-3" />}
+				onClick={this.controller.showCostomers}>
+				客户
+			</LMR>
 		</div>
 	}
 }
